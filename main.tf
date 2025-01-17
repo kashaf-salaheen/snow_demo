33d7b88f00d7e6e7f1c1674f0cff18afece92c04
kashaf-salaheen <170962665+kashaf-salaheen@users.noreply.github.com>
2025-01-17 14:20:52 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Retrieve the proper machine image for EC2 instance
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+# check existing instance count for naming tag
+data "aws_instances" "existing_simple_ec2" {
+  filter{
+    name = "tag:Group"
+    values = ["ServiceNow Simple EC2"]
+  }
+}
+
+
+locals {
+  simple_ec2_count = length(data.aws_instances.existing_simple_ec2.ids)
+}
+
+
+# Create AWS Compute Instance 
+resource "aws_instance" "ec2_test" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t2.micro"
+  //instance_type = "t2.nano"
+
+  tags = {
+    Name = "EC2 ServiceNow Test ${local.simple_ec2_count +1}"
+    Group = "ServiceNow Simple EC2"
+  }
+}
